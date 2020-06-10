70718ec711d0efcb46de1409fcb73f78cc9e7ca7
rootcanopy <dave.trees201@gmail.com>
2020-06-10 10:28:29 +0200

    contact form done in the home app. Form itself needs more styling but will do that when this app is finished and ready to be tested

--- home/forms.py
+++ home/forms.py
@@ -4,6 +4,28 @@ from django import forms
 # CONTACT FORM
 # REQUIRED = TRUE by DEFAULT
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length='100')
-    email = forms.EmailField(label='Your email', max_length='100')
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Your name'
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Your email'
+            }
+        )
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Your message'
+            }
+        )
+    )
+
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import ContactForm
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -11,13 +11,17 @@ def home_page(request):
     return render(request, 'home/index.html')
 
 
+# USER QUERY FORM
 def contact_page(request):
     """ view to return contact page """
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-
-    return render(request, 'home/contact.html')
+    form = ContactForm(request.POST)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+    # if request.method == "POST":
+    return render(request, 'home/contact.html', context)
 
 
 def about_page(request):
