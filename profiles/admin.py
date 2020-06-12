4e4c40667d1ee30b36837513b388a02a13f54d15
rootcanopy <dave.trees201@gmail.com>
2020-06-12 02:36:59 +0200

    testing is therapeutic? user app is cool for now I think, not sure which way to build the user profile with the order address and stuff but will figure that out along the way

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserChangeForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(User, CustomUserAdmin)
