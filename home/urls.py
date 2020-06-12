dee047e2220427b52a0a424c113682782fcc8fe6
rootcanopy <dave.trees201@gmail.com>
2020-06-12 19:57:21 +0200

    currently the slug:category select on the navbar isint working so will have to have a look at that tomorrow

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    # path('category/<slug:category_slug>',
+         #views.home_page, name='products_by_category'),
     path('contact/', views.contact_page, name='contact'),
     # path('success/', views.confirm_success, name='success'),
     path('about/', views.about_page, name='about'),
