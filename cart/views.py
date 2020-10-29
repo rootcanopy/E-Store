e8de79dcd6c22c5f90a485944a31be82995aa7ea
rootcanopy <dave.trees201@gmail.com>
2020-10-29 17:28:43 +0100

    bug in the shopping cart icon which im trying to fix. So far nearly there with the shopping cart although it needs to be restyled all around.. the functionality seems to be working the way it should

--- cart/views.py
+++ cart/views.py
@@ -7,29 +7,33 @@ from products.models import Product
 from django.contrib import messages
 
 
+# VIEW THE CART
 def view_cart(request):
     """ VIEW FOR SHOPPING CART """
     return render(request, 'cart/cart.html')
 
 
+# ADD TO THE CART
 def add_to_cart(request, product_id):
     """ ADD A PRODUCT AND QTY TO THE CART """
-    product = get_object_or_404(Product, id=product_id)
+    product = Product.objects.get(id=product_id)
     quantity = int(request.POST.get('quantity')) # this needs attn
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if product_id in list(cart.keys()):
         cart[product_id] += quantity
-        messages.success(request, 'Product quantity updated')
+        messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
+                                    
     else:
         cart[product_id] = quantity
-        messages.info(request, "Product added to your cart.")
+        messages.info(request, f'{product.name} Added to your bag')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
 
 
+# UPDATE THE CART VIEW
 def update_cart(request, product_id):
     """ ADJUST QTY IN CART """
     product = get_object_or_404(Product, id=product_id)
@@ -38,13 +42,19 @@ def update_cart(request, product_id):
 
     if quantity > 0:
         cart[product_id] = quantity
+        messages.success(request,
+                            (f'Updated {product.name} quantity to {cart[product_id]}'))
     else:
-        cart.pop[product_id]
+        cart.pop(product_id)
+        messages.success(request,            
+                            (f'Removed {product.name} '
+                            f'from your bag'))
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
 
+# REMOVE FROM THE CART
 def remove_from_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart = request.session.get('cart', {})
