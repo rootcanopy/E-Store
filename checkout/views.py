6bd4416646816639d965bdce77759a78f9cb2929
rootcanopy <dave.trees201@gmail.com>
2020-10-30 22:49:35 +0100

    stripe is finished.. after some figuring out

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,23 @@ import stripe
 import json
 
 
+@require_POST
+def cache_checkout_data(request):
+    try:    
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.' )
+        return HttpResponse(content=e, status=400)
+
+
 # TO DISPLAY THE CHECKOUT FORM
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -100,20 +117,3 @@ def checkout_success(request, order_id):
         'order': order,
     }
     return render(request, template, context)
-
-
-@require_POST
-def cache_checkout_data(request):
-    try:    
-        pid = request.POST.get('client_secret').split('_secret')[0]
-        stripe.api_key=settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
-            'cart': json.dumps(request.session.get('cart', {})),
-            'save_info': request.POST.get('save_info'),
-            'username': request.user,
-        })
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.' )
-        return HttpResponse(content=e, status=400)
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -28,7 +28,7 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook recieved: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_payment_failed(self, event):
         return HttpResponse(
             content=f'Webhook recieved: {event["type"]}',
